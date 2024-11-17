7656369558242f8527de58d5b95792601b8e1ff7
fhcnwuief <103618455+fhcnwuief@users.noreply.github.com>
2024-11-17 17:42:36 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 10.24 ms, Memory: 84.3 MB -BaekjoonHub

---
+++ Java/프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Solution {
+    public String solution(String[] seoul){
+        return "김서방은 "+ Arrays.asList(seoul).indexOf("Kim")+"에 있다";
+    }
+}
