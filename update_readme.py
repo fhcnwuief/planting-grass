fc904db0266ab2cac1955032142bc8a4e3405deb
fhcnwuief <103618455+fhcnwuief@users.noreply.github.com>
2024-11-23 21:04:17 +0900

    Update update_readme.py

--- update_readme.py
+++ update_readme.py
@@ -27,7 +27,7 @@ def update_readme(repo_path, counts):
         """디렉토리 구조를 Markdown 형식으로 반환"""
         # README.md 파일 발견 시 멈추고 디렉토리 구조를 Markdown 형식으로 반환
         structure = []
-         visited = set()  # 이미 방문한 디렉토리 추적
+        visited = set()  # 이미 방문한 디렉토리 추적
         
         for root, dirs, files in os.walk(base_dir):
             parts = root.split(os.sep)
