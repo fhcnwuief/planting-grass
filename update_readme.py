667696a4f24e2ada385b2418bef823994539f99f
fhcnwuief <103618455+fhcnwuief@users.noreply.github.com>
2024-11-23 20:26:44 +0900

    Update update_readme.py

--- update_readme.py
+++ update_readme.py
@@ -25,7 +25,7 @@ def count_files_by_platform(base_dir):
 def update_readme(repo_path, counts):
     def get_directory_structure(base_dir):
         """디렉토리 구조를 Markdown 형식으로 반환"""
-         """README.md 파일 발견 시 멈추고 디렉토리 구조를 Markdown 형식으로 반환"""
+        # README.md 파일 발견 시 멈추고 디렉토리 구조를 Markdown 형식으로 반환
         structure = []
         for root, dirs, files in os.walk(base_dir):
             parts = root.split(os.sep)
