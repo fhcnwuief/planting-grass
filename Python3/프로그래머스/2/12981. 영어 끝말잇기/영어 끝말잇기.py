ed347caf7f3226e44569cac0f8d8c926753086d1
fhcnwuief <103618455+fhcnwuief@users.noreply.github.com>
2025-04-27 17:13:14 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.09 ms, Memory: 9.35 MB -BaekjoonHub

---
+++ Python3/프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -0,0 +1,10 @@
+def solution(n, words):
+    # [번호, 차례]
+    answer = []
+    
+    for idx in range(1, len(words)):
+        if(words[idx-1][-1] != words[idx][0]) or (words[idx] in words[:idx]):
+            return [(idx%n) + 1,(idx//n)+1]
+    return [0,0]
+            
+
