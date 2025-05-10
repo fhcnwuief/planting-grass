b448d5917868efc074d8d0ebc52065fdf688a6a1
fhcnwuief <103618455+fhcnwuief@users.noreply.github.com>
2025-05-10 13:59:15 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.43 ms, Memory: 9.32 MB -BaekjoonHub

---
+++ Python3/프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -0,0 +1,12 @@
+def solution(n):
+    
+    # 피보나치 수여엉ㄹ
+    dp = [0] * (n+1)
+    
+    dp[0] = 1
+    dp[1] = 2
+    
+    for i in range(2,n):
+        dp[i] = dp[i-2] + dp[i-1]
+	
+    return dp[n-1] % 1234567
